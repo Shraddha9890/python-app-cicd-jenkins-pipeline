7e9a39799972b2aa68fd43fc85aa10b6c7307997
Shraddha_12 <shraddha.pund23@pcu.edu.in>
2026-02-04 14:49:41 +0530

    Fix tests: rename for pytest discovery and remove xmlrunner

---
+++ test_app.py
@@ -0,0 +1,32 @@
+import unittest
+import app
+import re
+
+class TestHello(unittest.TestCase):
+
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+
+    def test_hello(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status, '200 OK')
+
+        message = rv.data.decode()
+        start = re.search("<h1>", message).start()
+        end = re.search("</h1>", message).start()
+        message = message[start+4:end]
+
+        self.assertEqual(message, "hello brother, Lets have some fun!!")
+
+    def test_name(self):
+        name = 'alice'
+        rv = self.app.get(f'/{name}')
+        self.assertEqual(rv.status, '200 OK')
+
+        message = rv.data.decode()
+        start = re.search("<h1>", message).start()
+        end = re.search("</h1>", message).start()
+        message = message[start+4:end]
+
+        self.assertEqual(message, "alice: A nerd")
